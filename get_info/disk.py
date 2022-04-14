71a128681927b5e4b84a4e5eccf9b0d102103d33
Nikita Zaitsev <nikitazv90@gmail.com>
2022-04-14 07:50:41 +0300

    modify prepare info

---
+++ get_info/disk.py
@@ -0,0 +1,42 @@
+import psutil
+
+
+class Disk:
+    info = {}
+    template = '{total}\n'
+
+    def get(self):
+        self.info.update(total=psutil.disk_usage('/').total)
+        self.info.update(used=psutil.disk_usage('/').used)
+        self.info.update(free=psutil.disk_usage('/').free)
+        self.info.update(percent=psutil.disk_usage('/').percent)
+
+        self.info.update(device=[part.device for part in psutil.disk_partitions()])
+        self.info.update(mountpoint=[m.mountpoint for m in psutil.disk_partitions()])
+
+    def _prepare(self):
+        self.template += 'Total Disk:'
+        self.template += '{total}\n'
+
+        self.template += 'Used Disk:'
+        self.template += '{used}\n'
+
+        self.template += 'Free Disk:'
+        self.template += '{free}\n'
+
+        self.template += 'Percent of Disk Usage:'
+        self.template += '{percent}\n'
+
+        self.template += 'Device:\n'
+
+        for index in range(len(self.info['device'])):
+            self.template += '{device[' + str(index) + ']}\n'
+
+        self.template += 'Mountpoint:\n'
+
+        for index in range(len(self.info['mountpoint'])):
+            self.template += '{mountpoint[' + str(index) + ']}\n'
+
+    def show(self):
+        self._prepare()
+        print(self.template.format(**self.info))
